9bdc341011e64990eaeca9f6cdf427db0195d654
Holland Clarke <hollandclarke@Hollands-MBP.wowway.com>
2019-02-27 21:50:03 -0500

    Fixed up code and added comments

---
+++ TemperatureConverter/Extensions/ViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerExtensions.swift
+//  TemperatureConverter
+//
+//  Created by Holland Clarke on 2/27/19.
+//  Copyright © 2019 Holland Clarke. All rights reserved.
+//
+
+import Foundation
